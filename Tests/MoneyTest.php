e8ea241f0c14e0fae9682d38c255211e1d77e4a2
liam@greenhughes.com <>
2014-05-02 12:22:09 +0100

    Added example class and test.

---
+++ Tests/MoneyTest.php
@@ -0,0 +1,20 @@
+<?php
+require_once '../Money.php';
+class MoneyTest extends PHPUnit_Framework_TestCase
+{
+    // ...
+
+    public function testCanBeNegated()
+    {
+        // Arrange
+        $a = new Money(1);
+
+        // Act
+        $b = $a->negate();
+
+        // Assert
+        $this->assertEquals(-1, $b->getAmount());
+    }
+
+    // ...
+}
